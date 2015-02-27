c29fb60a711e733282b598b2d73d5cd6073e15c3
Trent <trent_thompson@rogers.com>
2015-02-27 17:55:42 -0500

    Initial Commit

---
+++ src/main/java/trentwt/moremusicmod/MoreMusicMod.java
@@ -0,0 +1,24 @@
+package trentwt.moremusicmod;
+
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public class MoreMusicMod {
+	
+	@EventHandler
+	public void preInitialization(FMLPreInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public void initialization(FMLInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public void postInitialization(FMLPostInitializationEvent event) {
+		
+	}
+}
