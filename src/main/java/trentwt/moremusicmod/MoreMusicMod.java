c95c729c94e20f4ceec7f942cbd4fb99c321d5b2
Trent <trent_thompson@rogers.com>
2015-02-27 18:44:14 -0500

    Added Handlers and Methods

--- src/main/java/trentwt/moremusicmod/MoreMusicMod.java
+++ src/main/java/trentwt/moremusicmod/MoreMusicMod.java
@@ -1,10 +1,13 @@
 package trentwt.moremusicmod;
 
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import trentwt.moremusicmod.utils.StringUtils;
 
+@Mod(modid = StringUtils.MOD_ID, name = StringUtils.MOD_NAME, version = StringUtils.MOD_VERSION)
 public class MoreMusicMod {
 	
 	@EventHandler
