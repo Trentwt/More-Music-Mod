5d5243486a13871799c8e58baf85b4d91da36c23
Trent <trent_thompson@rogers.com>
2015-02-27 18:46:41 -0500

    Added methods, mod declaration, handlers and StringUtils

---
+++ src/main/java/trentwt/moremusicmod/utils/StringUtils.java
@@ -0,0 +1,7 @@
+package trentwt.moremusicmod.utils;
+
+public class StringUtils {
+	public static final String MOD_ID = "moremusicmod";
+	public static final String MOD_NAME = "More Music Mod";
+	public static final String MOD_VERSION = "1.0";
+}
